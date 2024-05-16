150ac862c4b2a781a9a34d85311f7e075c567aaf
Goran Mahovlic <goran.mahovlic@gmail.com>
2024-05-16 21:35:02 +0200

    updating screen size

--- src/gui/document.h
+++ src/gui/document.h
@@ -191,7 +191,7 @@ enum PagesEnum {
     PAGE_ID_LOADER = 7
 };
 
-extern const uint8_t assets[67457];
+extern const uint8_t assets[67469];
 
 #elif defined(EEZ_PLATFORM_SIMULATOR)
 
@@ -378,7 +378,7 @@ enum PagesEnum {
     PAGE_ID_LOADER = 7
 };
 
-extern const uint8_t assets[67457];
+extern const uint8_t assets[67469];
 
 #endif
 
